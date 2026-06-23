fb61e838b0580df082dbb85445349d0d2696213a
jplande <lalandejeanpaul10@gmail.com>
2026-06-23 10:12:59 +0200

    exo 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+ weight: ["400", "600"],
+ style: ["normal"],
+ subsets: ["latin"],
+ display: "swap",
+ variable: "--font-poppins",}); 
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (<div >
+      <Component className={`${poppins.variable} font-sans`} {...pageProps} />
+    </div>
+  );
 }
